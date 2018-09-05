7c43f87ed150e19304d1c1126603f77f676098e0
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-09-05 13:50:36 +0200

    Fixed initializations / Created new classes

--- dmrg.py
+++ dmrg.py
@@ -11,16 +11,13 @@ import lanczos as lcz
 from scipy.linalg import eigh_tridiagonal as eigtrd
 from numpy.linalg import svd
 
-class NumPy_Tensors(object):
-    def __init__(self, D, H0, Hs, HN, lcz_k):
+class DMRG(object):
+    def __init__(self, D, d, H0, Hs, HN, lcz_k):
         ### DMRG parameters ###
-        ## N: Number of sites in the chain
-        N = len(Hs) + 2
         ## d: Physical dimension of each degree of freedom
-        self.d = len(H0)
-        ## D: MPS bond dimension (same for all sites)
-        self.D = D
+        ## D: List with MPS matrices dimensions (the list ignores first and last site where d=D)
         ## lcz_k: k-number for Lanczos
+        self.d, self.D = d, D
         
         ### Hamiltonian in MPO form ###
         ## DH: Hidden dimension for Hamiltonian
@@ -28,25 +25,49 @@ class NumPy_Tensors(object):
         ## Hs: Hamiltonian for middle chain: Complex (N-2, DH, DH, d, d)
         ## HN: Hamiltonian for last site: Complex (DH, d, d)
         
-        # Initialize MPS: Complex (N-2, D, D, d)
-        # First and last states are (D, d)
-        self.state0 = np.random.random(size=(self.d, D)) + 1j * np.random.random(size=(D, self.d))
-        self.states = np.random.random(size=(N-2, D, D, self.d)) + 1j * np.random.random(size=(N-2, D, D, self.d))
-        self.stateN = np.random.random(size=(self.d, D)) + 1j * np.random.random(size=(D, self.d))
+        ## Initialize MPS: List of complex (D, D, d) tensors
+        self.state = [np.random.random(size=(D1, D2, d)) + 1j * np.random.random(
+                size=(D1, D2, d)) for (D1, D2) in zip(D[:-1], D[1:])]
+        ## First and last states are (d, d) (for boundary D=d in normal form)
+        self.state = ([np.random.random(size=(d, d)) + 1j * np.random.random(size=(d, d))] +
+                       self.state + [np.random.random(size=(d, d)) + 1j * np.random.random(size=(d, d))])
+        
+        ## Normalize states to canonical form
+        self.normalize_states()
+        print('\nStates succesfully normalized!')
         
         ## Create Ops object
-        self.ops = Operations(D, H0, Hs, HN, lcz_k)
+        self.ops = Operations(D, H0, Hs, HN, lcz_k, self.plc)
         
         ## Open session
-        self.sess = tf.Session()
-        self.sess.run(tf.global_variables_initializer())
+        #self.sess = tf.Session()
+        #self.sess.run(tf.global_variables_initializer())
         
-    def first_update(self):
+    def normalize_states(self):
+        ## Start from right
+        U, S, self.state[-1] = svd(self.state[-1], full_matrices=False)
+        # (d x d)(d x d stored as (d,))(d x d)
+        self.state[-2] = np.einsum('abi,bc->aci', self.state[-2], U.dot(np.diag(S)))
+        # (D, D', d) = (D, D', d) x (D', D') (contraction of second index)
+        
+        ## Repeat for middle states
+        for i in range(len(self.D) - 1, 1, -1):
+            U, S, V = svd(self.state[i].reshape(self.D[i-1], self.D[i]*self.d), full_matrices=False)
+            self.state[i] = V.reshape(self.D[i-1], self.D[i], self.d)
+            self.state[i-1] = np.einsum('abi,bc->aci', self.state[i-1], U.dot(np.diag(S)))
+        
+        ## Normalize final state
+        U, S, V = svd(self.state[1].reshape(self.D[0], self.D[1]*self.d), full_matrices=False)
+        self.state[1] = V.reshape(self.D[0], self.D[1], self.d)
+        self.state[0] = np.einsum('bi,bc->ci', self.state[0], U.dot(np.diag(S)))
+        U, S, self.state[0] = svd(self.state[0], full_matrices=False)
+        
+    def initialize_R(self):
         ## Keep Rs in memory
-        self.R = np.zeros((self.ops.N - 1, self.D, self.ops.DH, self.D))
+        self.R = []
         
-        ## Calculate Rs
-        self.R[-1] = self.sess.run(self.ops.calc_R, feed_dict={self.ops.state_boundary : self.stateN})
+        ## Calculate first R (begining from right)
+        self.R.append(self.sess.run(self.ops.calc_R, feed_dict={self.ops.plc.state : self.state[-1]}))
         for i in range(self.ops.N - 3, -1 , -1):
             self.R[i] = self.sess.run(self.ops.calc_RLs[i][0], feed_dict={self.ops.R : self.R[i+1],
                   self.ops.state : self.states[i]})
@@ -59,30 +80,37 @@ class NumPy_Tensors(object):
         ## Update B and do SVD
         U, S, V = svd((V.dot(eig_vec[:, 0])).reshape(self.d, self.D*self.d))
         
-        ## Update states
-        state0 = 
+        
+class Placeholders(object):
+    def __init__(self):
+        self.state = tf.placeholder(dtype=tf.complex64)
+        self.R = tf.placeholder(dtype=tf.complex64)
+        self.L = tf.placeholder(dtype=tf.complex64)
+        
+class Hamiltonian(object):
+    def __init__(self, H0, Hs, HN):
+        self.left = tf.constant(H0, dtype=tf.complex64)
+        self.mid = tf.constant(Hs, dtype=tf.complex64)
+        self.right = tf.constant(HN, dtype=tf.complex64)
     
-
 class Operations(object):
     def __init__(self, D, H0, Hs, HN, lcz_k):
         self.N = len(Hs) + 2
         self.DH, d = H0.shape[:2]
         
-        ## Initialize Hamiltonian for TF graph
-        self.H0 = tf.constant(H0, dtype=tf.complex64)
-        self.Hs = tf.constant(Hs, dtype=tf.complex64)
-        self.HN = tf.constant(HN, dtype=tf.complex64)
+        ## Create placeholders object
+        self.plc = Placeholders()
+        ## Create hamiltonian object
+        self.H = Hamiltonian(H0, Hs, HN)
         
-        ## Create placeholders for MPS and R tensors
-        self.state_boundary = tf.placeholder(dtype=tf.complex64, shape=(D, d))
-        self.state = tf.placeholder(dtype=tf.complex64, shape=(D, D, d))
-        self.R = tf.placeholder(dtype=tf.complex64, shape=(D, self.DH, D))
-        self.L = tf.placeholder(dtype=tf.complex64, shape=(D, self.DH, D))
+        ## Determine if Hamiltonian is list or same for all middle sites
+        self.H_list_flag = (len(Hs.shape) >= 5)
         
         ## Create ops for R and L calculations/updates
-        self.calc_R, self.calc_L = self.RL_boundary_graph(self.HN), self.RL_boundary_graph(self.H0)
+        self.R_boundary, self.L_boundary = self.RL_boundary_graph(self.H.right), self.RL_boundary_graph(self.H.left)
+        if self.H_list_flag:
+            self.calc_RLs = [[self.R_graph(self.H.mid[i]), self.L_graph(self.H.mid[i])] for i in range(self.N-2)]
         ## Rs[i][0] for right, Rs[i][1] for left ##
-        self.calc_RLs = [[self.R_graph(i), self.L_graph(i)] for i in range(self.N-2)]
         
         ## Create Lanczos ops
         self.lanczos_boundary0 = tf.contrib.solvers.lanczos.lanczos_bidiag(
@@ -108,15 +136,15 @@ class Operations(object):
         R = tf.einsum('bij,cj->bci', hamiltonian, self.state_boundary)
         return tf.einsum('bci,ai->abc', R, tf.conj(self.state_boundary))
     
-    def R_graph(self, i):
+    def R_graph(self, Hi):
         Rnew = tf.einsum('abc,fcj->abfj', self.R, self.state)
-        Rnew = tf.einsum('ebij,abfj->aefi', self.Hs[i], Rnew)
+        Rnew = tf.einsum('ebij,abfj->aefi', Hi, Rnew)
         return tf.einsum('adi,aefi->def', tf.conj(self.state), Rnew)
         #also changed the indices in einsum to take into account the dagger
     
-    def L_graph(self, i):
+    def L_graph(self, Hi):
         Lnew = tf.einsum('def,fcj->decj', self.L, self.state)
-        Lnew = tf.einsum('ebij,decj->dbci', self.Hs[i], Lnew)
+        Lnew = tf.einsum('ebij,decj->dbci', Hi, Lnew)
         return tf.einsum('adi,dbci->abc', tf.conj(self.state), Lnew)
         #also changed the indices in einsum to take into account the dagger
 
