ab487d69996b732120774761fef78059e30bee44
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-09-06 12:09:39 +0200

    Checking Lanczos objects

--- graphs.py
+++ graphs.py
@@ -9,10 +9,14 @@ import tensorflow as tf
    
 class Operations(object):
     def __init__(self, D, H0, Hs, HN, lcz_k):
+        ### Hamiltonian in MPO form ###
+        ## DH: Hidden dimension for Hamiltonian
+        ## H0: Hamiltonian for first site: Complex (DH, d, d)
+        ## Hs: Hamiltonian for middle chain: Complex (N-2, DH, DH, d, d)
+        ## HN: Hamiltonian for last site: Complex (DH, d, d)
+        self.D = D        
         self.N = len(Hs) + 2
-        self.DH, self.d = H0.shape[:2]
-        self.D = D
-        
+                
         ## Create placeholders object
         self.plc = Placeholders()
         ## Create hamiltonian object
@@ -38,27 +42,27 @@ class Operations(object):
     def create_lanczos_ops(self, lcz_k):
         if self.H_list_flag:
             self.lanczos0 = tf.contrib.solvers.lanczos.lanczos_bidiag(
-                    operator=Lanczos_Ops_Boundary(self.d, self.d, self.H.left, self.H.mid[0], self.plc.R), 
+                    operator=Lanczos_Ops_Boundary(self.H.left, self.H.mid[0], self.plc.R), 
                     k=lcz_k, name="lanczos_bidiag_left")
             self.lanczosN = tf.contrib.solvers.lanczos.lanczos_bidiag(
-                    operator=Lanczos_Ops_Boundary(self.d, self.d, self.H.mid[-1], self.H.right, self.plc.L), 
+                    operator=Lanczos_Ops_Boundary(self.H.mid[-1], self.H.right, self.plc.L), 
                     k=lcz_k, name="lanczos_bidiag_right")
             
             self.lanczosM = [tf.contrib.solvers.lanczos.lanczos_bidiag(
-                    operator=Lanczos_Ops(self.D[i], self.D[i+1], self.H.mid[i], self.H.mid[i+1], 
+                    operator=Lanczos_Ops(self.D[i-1], self.D[i+1], self.H.mid[i], self.H.mid[i+1], 
                                          self.plc.L, self.plc.R), 
                     k=lcz_k, name="lanczos_bidiag_mid%d"%i) for i in range(self.N - 3)]
         
         else:
             self.lanczos0 = tf.contrib.solvers.lanczos.lanczos_bidiag(
-                    operator=Lanczos_Ops_Boundary(self.d, self.d, self.H.left, self.H.mid, self.plc.R), 
+                    operator=Lanczos_Ops_Boundary(self.d, self.H.left, self.H.mid, self.plc.R), 
                     k=lcz_k, name="lanczos_bidiag_left")
             self.lanczosN = tf.contrib.solvers.lanczos.lanczos_bidiag(
-                    operator=Lanczos_Ops_Boundary(self.d, self.d, self.H.mid, self.H.right, self.plc.L), 
+                    operator=Lanczos_Ops_Boundary(self.d, self.H.mid, self.H.right, self.plc.L), 
                     k=lcz_k, name="lanczos_bidiag_right")
             
             self.lanczosM = [tf.contrib.solvers.lanczos.lanczos_bidiag(
-                    operator=Lanczos_Ops(self.D[i], self.D[i+1], self.H.mid, self.H.mid, 
+                    operator=Lanczos_Ops(self.D[i-1], self.D[i+1], self.H.mid, self.H.mid, 
                                          self.plc.L, self.plc.R), 
                     k=lcz_k, name="lanczos_bidiag_mid%d"%i) for i in range(self.N - 3)]
         
@@ -69,13 +73,12 @@ class Operations(object):
     def R_graph(self, Hi):
         x = tf.einsum('abc,fcj->abfj', self.plc.R, self.plc.state)
         x = tf.einsum('ebij,abfj->aefi', Hi, x)
-        return tf.einsum('adi,aefi->def', tf.conj(self.plc.state), x)
-        #also changed the indices in einsum to take into account the dagger
+        return tf.einsum('dai,aefi->def', tf.conj(self.plc.state), x)
     
     def L_graph(self, Hi):
         x = tf.einsum('def,fcj->decj', self.plc.L, self.plc.state)
         x = tf.einsum('ebij,decj->dbci', Hi, x)
-        return tf.einsum('adi,dbci->abc', tf.conj(self.plc.state), x)
+        return tf.einsum('dai,dbci->abc', tf.conj(self.plc.state), x)
         #also changed the indices in einsum to take into account the dagger
 
 class Placeholders(object):
@@ -91,23 +94,26 @@ class Hamiltonian(object):
         self.right = tf.constant(HN, dtype=tf.complex64)
 
 class Lanczos_Ops(object):
-    def __init__(self, D, d, H1, H2, L, R):
-        self.D, self.d = D, d
-        self.dims = D*D*d*d
+    def __init__(self, DL, DR, H1, H2, L, R):
+        self.DL, self.DR = DL, DR
+        self.d = H1.shape[-1]
+        self.dims = DL * DR * self.d * self.d
         self.shape = [self.dims, self.dims]
         self.dtype = tf.complex64
-        
+
         self.H1, self.H2 = H1, H2
         self.L, self.R = L, R
         
     def apply(self, x):
-        B = tf.reshape(x, shape=(self.D, self.D, self.d, self.d))
+        B = tf.reshape(x, shape=(self.DL, self.DR, self.d, self.d))
         LB = tf.einsum('abc,cgkl->abgkl', self.L, B)
         LB = tf.einsum('abgkl,bdik->adgil', LB, self.H1)
         LB = tf.einsum('adgil,dfjl->afgij', LB, self.H2)
-        LB = tf.einsum('afgij,efg->aeij', LB, self.R)
+        LB = tf.einsum('afgij,efg->aije', LB, self.R)
+        ## Final indices in order DL, d, d, DR
         return tf.reshape(LB, shape=(self.dims,))
-        
+    
+    ### CHECK APPLY ADJOINT! ###
     def apply_adjoint(self, x):
         B = tf.reshape(x, shape=(self.D, self.D, self.d, self.d))
         B = tf.conj(tf.transpose(B, [1, 0, 3, 2]))
@@ -118,9 +124,9 @@ class Lanczos_Ops(object):
         return tf.conj(tf.reshape(tf.transpose(LB, [1, 0, 3, 2]), shape=(self.dims,)))
     
 class Lanczos_Ops_Boundary(object):
-    def __init__(self, D, d, H1, H2, LR):
-        self.D, self.d = D, d
-        self.dims = D*d*d
+    def __init__(self, H1, H2, LR):
+        self.d = H1.shape[-1]
+        self.dims = self.d**3
         self.shape = [self.dims, self.dims]
         self.dtype = tf.complex64
         
@@ -128,16 +134,18 @@ class Lanczos_Ops_Boundary(object):
         self.LR = LR
         
     def apply(self, x):
-        B = tf.reshape(x, shape=(self.D, self.d, self.d))
-        LB = tf.einsum('abc,cij->abij', self.L, B)
+        B = tf.reshape(x, shape=(self.d, self.d, self.d))
+        LB = tf.einsum('abc,cij->abij', self.LR, B)
         LB = tf.einsum('abij,bdki->adkj', LB, self.H1)
         LB = tf.einsum('adkj,dlj->akl', LB, self.H2)
         return tf.reshape(LB, shape=(self.dims,))
-        
+    
+    ### CHECK APPLY ADJOINT! ###
     def apply_adjoint(self, x):
-        B = tf.reshape(x, shape=(self.D, self.d, self.d))
+        B = tf.reshape(x, shape=(self.d, self.d, self.d))
         B = tf.conj(tf.transpose(B, [1, 0, 3, 2]))
-        LB = tf.einsum('abc,cij->abij', self.L, B)
+        LB = tf.einsum('abc,cij->abij', self.LR, B)
         LB = tf.einsum('abij,bdki->adkj', LB, self.H1)
         LB = tf.einsum('adkj,dlj->akl', LB, self.H2)
         return tf.conj(tf.reshape(tf.transpose(LB, [1, 0, 3, 2]), shape=(self.dims,)))
+    
