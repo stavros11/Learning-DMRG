3b4bfba73a01d96eb50b5b09e16ca11a21126303
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-09-10 16:46:08 +0200

    Finally made this work

--- TFIM1D_ML.py
+++ TFIM1D_ML.py
@@ -14,91 +14,83 @@ from itertools import product
 #################################################
 
 def classical_magnetization(states):
-    #return np.sum(2 * states - 1, axis=1)
-    return np.sum(states, axis=1)
+    return np.sum(1 - 2 * states, axis=1)
 
 def classical_energy(states, pbc=False):
-    #x = 2 * states - 1
-    x = states
+    x = 1 - 2 * states
     energy = np.sum(x[:, 1:] * x[:, :-1], axis=1)
     if pbc:
         energy+= x[:, 0] * x[:, 1]
     return -energy
 
-class Machine(object):
-    def __init__(self, N):
-        self.N = N
-        
-        #self.psi = tf.Variable(np.random.normal(loc=0.0, scale=0.001, size=(2**N,)), 
-        #                       dtype=tf.float32)
-        self.psi = tf.Variable(np.load('TFIM1D_N10H1_GS.npy'), dtype=tf.float32)
-
 class Trainer(object):
-    def __init__(self, machine, h, pbc=False):
+    def __init__(self, N, h, pbc=False):
         ## Number of sites
-        self.N = machine.N
-        ## Machine that gives psi(sigma)
-        self.machine = machine
+        self.N = N
         ## Magnetic field
         self.h = h
         ## Periodic Boundary Conditions
         self.pbc = pbc
         
         ## Create all possible states
-        self.states = np.array([list(x) for x in product([1, -1], repeat=self.N)])
+        self.states = np.array([list(x) for x in product([0, 1], repeat=self.N)])
         
         ## Create classical quantity required for energy calculation
-        self.classical = tf.constant((classical_energy(self.states, pbc=self.pbc)
-        - self.h * classical_magnetization(self.states)), dtype=tf.float32)
-    
-        ## Selection indices for cross term
-        self.selection = np.array([np.argwhere(self.states[:,i] == -1) for i in range(self.N)])[:,:,0]
+        self.classical = tf.constant(classical_energy(self.states, pbc=self.pbc), dtype=tf.float32)
+        self.to_decimal = 2**np.arange(self.N-1, -1, -1)
+        
+        ## Create wavefunction
+        #self.psi = tf.Variable(np.load('TFIM1D_N10H1_GS.npy'), dtype=tf.float32)
+        self.psi = tf.Variable(np.random.normal(loc=0.0, scale=0.1, size=(2**N,)), dtype=tf.float32)
     
         ## Create energy and training ops
-        self.energy_op = self.energy_graph()
-        self.train_op = self.training_graph()
-        
+        self.energy_graph()
+        self.training_graph()
+                
     def energy_graph(self):
-        psi2 = tf.multiply(self.machine.psi, self.machine.psi)
-        print(psi2)
+        psi2 = tf.multiply(self.psi, self.psi)
         self.Z = tf.reduce_sum(psi2)
         
-        classical_term = tf.reduce_sum(tf.multiply(psi2, self.classical))
-        print(classical_term)
-        cross_term = tf.reduce_sum(tf.multiply(
-                    tf.gather(self.machine.psi, self.selection[0]),
-                    tf.gather(self.machine.psi, self.selection[0] - 2**(9))))
+        interaction = tf.reduce_sum(tf.multiply(psi2, self.classical))
+        field = self.field_term(0)
         for i in range(1, self.N):
-            cross_term = tf.add(cross_term, tf.reduce_sum(tf.multiply(
-                    tf.gather(self.machine.psi, self.selection[i]),
-                    tf.gather(self.machine.psi, self.selection[i] - 2**(9-i)))))
+            field = tf.add(field, self.field_term(i))
+
+        self.energy_op = (interaction - self.h * field) / self.Z
+    
+    def field_term(self, i):
+        flipped_states = np.copy(self.states)
+        flipped_states[:,i] = (self.states[:,i] == 0).astype(np.int)
+        ind = flipped_states.dot(self.to_decimal)
         
-        print(cross_term)
-        return (classical_term - 2 * self.h * cross_term) / self.Z
+        return tf.reduce_sum(tf.multiply(self.psi,
+                                         tf.gather(self.psi, ind)))
     
     def training_graph(self):
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-        return optimizer.minimize(self.energy_op)
+        self.train_op = optimizer.minimize(self.energy_op)
     
     def train_early_stop(self, delta=1e-8, patience=20, en_calc=100, message=2000):
         #fd = {self.machine.states : self.states}
         epoch, counter = 0, 0
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            energies, psis = [sess.run(self.energy_op)], [sess.run(self.machine.psi)]
+            energies, psis = [sess.run(self.energy_op)], [sess.run(self.psi)]
             
             while True:
                 sess.run(self.train_op)
                 
                 if epoch % en_calc == 0:
+                    psis.append(sess.run(self.psi))
                     energies.append(sess.run(self.energy_op))
-                    psis.append(sess.run(self.machine.psi))
                     
                     if epoch % message == 0:
                         print('Epoch: %d  -  Energy: %.10f\n'%(epoch, energies[-1]))
                         
                 if np.abs(energies[-1] - energies[-2]) < delta:
                     counter += 1
+                else:
+                    counter = 0
                     
                 if counter > patience:
                     break
@@ -106,3 +98,23 @@ class Trainer(object):
                 epoch += 1
         
         return energies, psis
+    
+### Debugging ###
+tr = Trainer(N=10, h=1.0)
+
+en_pred, psis = tr.train_early_stop(patience=10, en_calc=1, message=50)
+
+#### Calculate normal quantities ###
+#from TFIM1D_ED import Ham
+#cl = classical_energy(tr.states) - tr.h * classical_magnetization(tr.states) / np.sqrt(2)
+#
+#t1_th, t2_th, Z_th = np.zeros(len(psis)), np.zeros(len(psis)), np.zeros(len(psis))
+#en_th, en_thc = np.zeros(len(psis)), np.zeros(len(psis))
+#for (i, x) in enumerate(psis):
+#    Z_th[i] = np.sum(x * x)
+#    en_th[i] = np.sum((x * Ham.dot(x)) / Z_th[i])
+#    t1_th[i] = np.sum(x*x*cl)
+#    for j in range(tr.N):
+#        t2_th[i] += np.sum(x[tr.selection[j]] * x[tr.selection[j] + 2**(9-j)])
+#    
+#    en_thc[i] = (t1_th[i] - np.sqrt(2.0) * tr.h * t2_th[i]) / Z_th[i]
--- tf_test.py
+++ tf_test.py
@@ -5,21 +5,35 @@ Created on Mon Sep 10 12:15:15 2018
 @author: Admin
 """
 
-import tensorflow as tf
+import numpy as np
+from itertools import product
+from TFIM1D_ED import Ham
 
-x = tf.Variable(1., dtype=tf.float32)
-y = tf.constant(2., dtype=tf.float32)
+N = 10
+h = 1
+eigvals, eigvecs = np.linalg.eigh(Ham)
 
-loss = 0
-for p in range(2, 6):
-    loss += tf.pow(x, p)
+psi = eigvecs[:,2]
 
-train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
+def classical_energy(states, pbc=False):
+    x = 1 - 2 * states
+    energy = np.sum(x[:, 1:] * x[:, :-1], axis=1)
+    if pbc:
+        energy+= x[:, 0] * x[:, 1]
+    return -energy
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    
-    for i in range(200):
-        print(sess.run(x), sess.run(loss))
-        sess.run(train)
-        
+states = np.array([list(x) for x in product([0, 1], repeat=N)])
+to_decimal = 2**np.arange(N-1, -1, -1)
+cl = classical_energy(states)
+
+en_theory = np.sum(psi * Ham.dot(psi))
+
+cross_term = 0
+for i in range(N):
+    flipped_states = np.copy(states)
+    flipped_states[:,i] = (states[:,i] == 0).astype(np.int)
+    cross_term += np.sum(psi * psi[flipped_states.dot(to_decimal)])
+
+en_mine = (np.sum(psi*psi*cl) - h * cross_term) / np.sum(psi*psi)
+
+print(en_theory, en_mine)
