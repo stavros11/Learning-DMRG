2ea6257fb2a738453ac420f3b42cdbe23b8b7235
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-09-11 13:46:18 +0200

    Made it a module

--- TFIM1D_ED.py
+++ TFIM1D_ED.py
@@ -16,31 +16,34 @@ def kron_list(matrices_list):
         return term
     else:
         return kron_list([term] + matrices_list[2:])
-
-N = 10
-h = 1
-
-### Create Hamiltonian Matrix ###
-Ham = np.zeros([2**N, 2**N])
-HB = np.zeros([2**N, 2**N])
-
-## Add interactions to Hamiltonian
-interaction = [np.kron(pauli_Z, pauli_Z)]
-identities = [np.eye(2) for i in range(N-2)]
-
-for i in range(N-1):
-    Ham += kron_list(identities[:i] + interaction + identities[i:])
     
-## Add PBC term
-#Ham += kron_list([pauli_Z] + identities + [pauli_Z])
+def create_hamiltonian(N=10, h=1.0, pbc=False):
+    ### Create Hamiltonian Matrix ###
+    Ham = np.zeros([2**N, 2**N])
+    HB = np.zeros([2**N, 2**N])
+    
+    ## Add interactions to Hamiltonian
+    interaction = [np.kron(pauli_Z, pauli_Z)]
+    identities = [np.eye(2) for i in range(N-2)]
+    
+    for i in range(N-1):
+        Ham += kron_list(identities[:i] + interaction + identities[i:])
+    
+    ## Add PBC term
+    if pbc: 
+        Ham += kron_list([pauli_Z] + identities + [pauli_Z])
 
-## Add field interactions
-identities.append(np.eye(2))
-for i in range(N):
-    HB += kron_list(identities[:i] + [pauli_X] + identities[i:])
-Ham += h * HB
-
-Ham = - Ham * (np.abs(Ham) > 1e-8).astype(np.int)
-en = np.linalg.eigvalsh(Ham)
-print(Ham.shape)
-print(en[:10])
+    ## Add field interactions
+    identities.append(np.eye(2))
+    for i in range(N):
+        HB += kron_list(identities[:i] + [pauli_X] + identities[i:])
+    Ham += h * HB
+    
+    ## Get rid of small values
+    return - Ham * (np.abs(Ham) > 1e-8).astype(np.int)
+    
+    
+#Ham = create_hamiltonian(N=9, h=1.0)  
+#en = np.linalg.eigvalsh(Ham)
+#print(Ham.shape)
+#print(en[:10])
