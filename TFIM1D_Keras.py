c7d49e2bd63612ca630c4dd6ccb37cbc3d5f309b
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-09-11 13:46:33 +0200

    Only load

--- TFIM1D_Keras.py
+++ TFIM1D_Keras.py
@@ -8,9 +8,10 @@ Created on Mon Sep 10 17:31:36 2018
 import numpy as np
 from itertools import product
 from os import path
+from TFIM1D_ED import create_hamiltonian
 
 from keras.models import Sequential
-from keras.layers import Conv1D, InputLayer
+from keras.layers import Conv1D, InputLayer, Lambda
 import keras.backend as K
 
 #################################################
@@ -24,7 +25,7 @@ def classical_energy(states, pbc=False):
         energy+= x[:, 0] * x[:, 1]
     return -energy
 
-class Trainer(object):
+class Predictor(object):
     def __init__(self, N, h, pbc=False):
         ## Number of sites
         self.N = N
@@ -47,9 +48,16 @@ class Trainer(object):
     def machine(self):
         self.model = Sequential()
         self.model.add(InputLayer(input_shape=self.states.shape[1:]+(1,)))
-        self.model.add(Conv1D(64, 5, activation='relu'))
         self.model.add(Conv1D(32, 4, activation='relu'))
-        self.model.add(Conv1D(1, 3, activation='sigmoid'))
+        self.model.add(Conv1D(16, 3, activation='relu'))
+        self.model.add(Conv1D(8, 3, activation='sigmoid'))
+        #dim = K.get_variable_shape(self.model.layers[-1].output)[1]
+        #self.model.add(AveragePooling1D(pool_size=dim))
+        #self.model.add(GlobalAveragePooling1D())
+        self.model.add(Lambda(lambda x : K.mean(x, axis=(1,2))))
+        
+    def predict(self):
+        return self.model.predict(self.states[:, :, np.newaxis])
                 
 #    def energy(self, psi):
 #        psi2 = psi * psi
@@ -71,22 +79,24 @@ class Trainer(object):
 #    
 #    def loss(self, y_true, y_pred):
 #        return self.energy(y_pred[:, 0, 0])
-    
-    def save_weights(self, folder):
-        for (i, l) in enumerate(self.model.layers):
-            np.save(path.join(folder, 'WeightsLayer%d.npy'%i), K.eval(l.weights[0]))
-            np.save(path.join(folder, 'BiasesLayer%d.npy'%i), K.eval(l.weights[1]))
+#    
+#    def save_weights(self, folder):
+#        for (i, l) in enumerate(self.model.layers):
+#            np.save(path.join(folder, 'WeightsLayer%d.npy'%i), K.eval(l.weights[0]))
+#            np.save(path.join(folder, 'BiasesLayer%d.npy'%i), K.eval(l.weights[1]))
             
     def load_model(self, folder):
-        for i in range(len(self.model.layers)):
+        for i in range(len(self.model.layers[:-1])):
             self.model.layers[i].set_weights([np.load(path.join(folder, 'WeightsLayer%d.npy'%i)),
                                               np.load(path.join(folder, 'BiasesLayer%d.npy'%i))])
         print('\nWeights loaded.\n')
         
-        
-tr = Trainer(N=10, h=1.0)
-tr.compiler('CNN1')
 
-psi = tr.model.predict(tr.states[:, :, np.newaxis])[:,0,0]
+N = 9
+tr = Predictor(N=N, h=1.0)
+tr.compiler('CNN_N8_pool')
 
+psi = tr.predict()
+
+Ham = create_hamiltonian(N=N, h=1.0)
 print(np.sum(psi * (Ham.dot(psi))) / np.sum(psi * psi))
